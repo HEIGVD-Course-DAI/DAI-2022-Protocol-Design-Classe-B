e85add8ed6f52cc8b72dc71329d15d417b7cea88
Florian <florian.conti@heig-vd.ch>
2022-10-31 14:48:23 +0100

    Server part done but have to see if it works with the client

--- ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
+++ ServerSingleThread/src/main/java/ch/heigvd/api/calc/Server.java
@@ -3,6 +3,7 @@ package ch.heigvd.api.calc;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -32,6 +33,22 @@ public class Server {
          *  For a new client connection, the actual work is done by the handleClient method below.
          */
 
+
+        Socket clientSocket = null;
+
+        try{
+            ServerSocket serverSocket = new ServerSocket(9999);
+
+            while(true){
+                clientSocket = serverSocket.accept();
+                handleClient(clientSocket);
+                clientSocket.close();
+            }
+        }
+        catch (IOException ex){
+            LOG.log(Level.SEVERE, ex.getMessage());
+        }
+
     }
 
     /**
@@ -50,6 +67,84 @@ public class Server {
          *     - Handle the message
          *     - Send to result to the client
          */
+
+        BufferedReader is = null;
+        BufferedWriter os = null;
+
+        char[] operationsList = {'+', '-', '/', '*'};
+
+        try{
+            is = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+            os = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
+
+            LOG.log(Level.INFO, "A new client has arrived");
+
+            os.write("Hello client, i can do those operations : ");
+            for (char c : operationsList) os.write(c + " ");
+
+            os.flush();
+
+            String line;
+            while((line = is.readLine()) != null){
+                String[] calculs = line.split(";");
+
+                if (calculs.length != 3){
+                    os.write("the calculation should look like this : operation;arg1;arg2 ! Try again :\r\n");
+                    os.flush();
+                    continue;
+                }
+
+                char operation = calculs[0].charAt(0);
+                double arg1    = Double.parseDouble(calculs[1]);
+                double arg2    = Double.parseDouble(calculs[2]);
+
+                double result = 0;
+
+                switch (operation){
+                    case '+':
+                        result = arg1 + arg2;
+                        break;
+                    case '-':
+                        result = arg1 - arg2;
+                        break;
+                    case '/':
+                        result = arg1 / arg2;
+                        break;
+                    case '*':
+                        result = arg1 * arg2;
+                        break;
+                    default:
+                        break;
+                }
+
+                os.write(result + "\r\n");
+                os.write("Try again if you want :\r\n");
+                os.flush();
+            }
+
+            LOG.info("Close connection with client");
+            is.close();
+            os.close();
+
+        }catch (IOException ex) {
+            LOG.log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                os.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                clientSocket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
 
     }
 }
